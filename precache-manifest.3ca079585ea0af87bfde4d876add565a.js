4db7f425f9de29df6f0ed04f4423496379787c2e
Alexandr <e7000@ukr.net>
2020-06-22 02:33:45 +0300

    Updates

---
+++ precache-manifest.3ca079585ea0af87bfde4d876add565a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd14b77a9d7824e5e3bec9fb823958ad",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "9878db8e25b54d2140ef",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.33ad9bf3.chunk.css"
+  },
+  {
+    "revision": "c23e8dbf74024f2f09b1",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.7c4e5d86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.7c4e5d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9878db8e25b54d2140ef",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.9dbe8e3e.chunk.js"
+  },
+  {
+    "revision": "97ee969fce12aed98c62",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.5dea50d2.js"
+  }
+]);
